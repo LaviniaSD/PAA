791b38cee695abc64dcc450f68d4199ef1c5cc91
Lavínia Silva Dias <l4viniadias510@gmail.com>
2023-09-25 10:13:49 -0300

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,28 @@
+
+#read_csv function  
+def read_csv(file,separator=',',encoding='utf-8'):
+    pass
+
+#show function
+def show(start=0,end=100):
+    pass
+
+#insert row function
+def insert_row(row):
+    pass
+
+#delete row function
+def delete_row(column,value):
+    pass
+
+#search function
+def search(column,value):
+    pass
+
+#sort function
+def sort(column, direction='asc'):
+    pass
+
+#select count function
+def select_count(i,j):
+    pass
