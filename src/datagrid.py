425cc20efab2600995d79b5547c69c312a8c5369
George D <georgedutra661@gmail.com>
2023-09-25 15:57:40 -0300

    Documente as classes

--- src/datagrid.py
+++ src/datagrid.py
@@ -1,12 +1,23 @@
 
 class DataGrid():
+    """"Objeto que armazena um datagrid de negócios
+    """"
     def __init__(self):
+        """Inicializa um DataGrid vazio
+        """
         self.list = []
 
 class Event():
+    """Objeto que armazena uma linha de um DataGrid
+    """
     def __init__(self, dict = None, **kwargs):
-        if dict == None: dict = kwargs
+        """Inicializa um evento a partir dos dados passados nominalmente, através de um dicionário ou diretamente com keyword arguments 
         
+        Args:
+            dict (dict, optional): dicionário contendo dados de uma linha do DataGrid, com o nome das colunas como chaves, e as entradas do DataGrid como valores. Defaults to None.
+        """
+        if dict == None: dict = kwargs
+
         self.id = dict["id"]
         self.owner_id = dict["owner_id"]
         self.creation_date = dict["creation_date"]
