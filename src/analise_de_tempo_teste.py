12d8546c004eacde9413bff6290f8139d91c05f6
AlmirFonseca <almirfonseca60@gmail.com>
2023-10-09 13:33:32 -0300

    Atualize a analise de tempo dos sorts, gerando um Pandas Dataframe com os resultados dos testes executados

--- src/analise_de_tempo_teste.py
+++ src/analise_de_tempo_teste.py
@@ -1,46 +1,70 @@
-import datagrid as dg
-import timer
-import pandas as pd
 import sys
+import timer
+
+import pandas as pd
+
+import datagrid as dg
+
+from exceptions import InvalidColumnError
+
 sys.setrecursionlimit(10**6) 
 
 # Método para ver todas as colunas do dataframe
 pd.set_option("display.max_columns", None)
 
-# Colunas do datagrid para serem analisadas
+def analise_sort(datagrids, columns_datagrid, sorts):
+
+    # Inicializa um Pandas DataFrame para armazenar os resultados
+    results = pd.DataFrame(columns=["Função", "Coluna", "Tamanho da amostra", "Tempo de execução"])
+
+    # Itera sobre cada datagrid
+    for datagrid in datagrids:
+
+        # Cria uma cópia do datagrid para ser usado nas funções de ordenação
+        datagrid_copy = datagrid.copy()
+
+        # Itera sobre cada função de ordenação
+        for sort_type in sorts:
+            # Itera sobre cada coluna do datagrid
+            for column in columns_datagrid:
+                try:
+                    datagrid_copy.sort(column=column, direction="asc", strategy=sort_type)
+                    print(f"Função: Sort({sort_type}) | Coluna: {column} | Tempo de execução: {timer.get_execution_time(sort_type)} ns")
+                except InvalidColumnError:
+                    pass # Erros relacionados a colunas não suportadas são ignorados
+                except Exception as error:
+                    print(f"Erro inesperado na função: Sort({sort_type}): \n {error}")
+                    pass
+                else:
+                    # Armazena o resultado da execução da função em um Pandas DataFrame
+                    results = results._append({"Função": sort_type, "Coluna": column, "Tamanho da amostra": datagrid.size, "Tempo de execução": timer.get_execution_time(sort_type)}, ignore_index=True)
+
+    # Retorna o dataframe com os resultados
+    return results
+
+# Datagrids a serem testados
+samples = []
+samples_paths=["data\dados_gerados_big.csv", 
+            #    "data\dados_gerados_big_1.csv", 
+            #    "data\dados_gerados_big_2.csv", 
+            #    "data\dados_gerados_big_3.csv", 
+            #    "data\dados_gerados_big_4.csv", 
+            #    "data\dados_gerados_big_5.csv", 
+            #    "data\dados_gerados_big_6.csv", 
+            #    "data\dados_gerados_big_7.csv"
+            ]
+
+# Carrega os datagrids a serem bases para os testes
+for file_path in samples_paths:
+    sample = dg.DataGrid()
+    sample.read_csv(file_path, separator=",")
+    samples.append(sample)
+
+# Colunas do datagrid para serem testadas
 columns_datagrid = ["id", "owner_id", "creation_date", "count", "name", "content", "timestamp"]
 
-# Iniciando datagrid vazio
-sample = dg.DataGrid()
-sample_0 = dg.DataGrid()
-sample_1 = dg.DataGrid()
-sample_2 = dg.DataGrid()
-sample_3 = dg.DataGrid()
-sample_4 = dg.DataGrid()
-sample_5 = dg.DataGrid()
-sample_6 = dg.DataGrid()
-sample_7 = dg.DataGrid()
-# Populando datagrid
-dg.DataGrid.read_csv(sample, "data\sample.csv", separator=",")
-dg.DataGrid.read_csv(sample_0, "data\dados_gerados_big.csv", separator=",")
-dg.DataGrid.read_csv(sample_1, "data\dados_gerados_big_1.csv", separator=",")
-dg.DataGrid.read_csv(sample_2, "data\dados_gerados_big_2.csv", separator=",")
-dg.DataGrid.read_csv(sample_3, "data\dados_gerados_big_3.csv", separator=",")
-dg.DataGrid.read_csv(sample_4, "data\dados_gerados_big_4.csv", separator=",")
-dg.DataGrid.read_csv(sample_5, "data\dados_gerados_big_5.csv", separator=",")
-dg.DataGrid.read_csv(sample_6, "data\dados_gerados_big_6.csv", separator=",")
-dg.DataGrid.read_csv(sample_7, "data\dados_gerados_big_7.csv", separator=",")
-files_datagrid = [sample_0, sample_1, sample_2, sample_3, sample_4, sample_5, sample_6, sample_7]
-files = [sample]
-def analise_sort(files_datagrid, columns_datagrid, *args):
-    # Obtendo tempo de tempo
-    for data_file in files_datagrid:
-        for column in columns_datagrid:
-            for func in args:
-                try:
-                    func(data_file, column=column, direction="asc")
-                except TypeError:
-                    pass
+# Estratégias de ordenação a serem testadas
+sorts = ["insertion_sort", "selection_sort", "merge_sort", "quick_sort", "heap_sort", "radix_sort"]
 
-analise_sort([sample], columns_datagrid, (dg.DataGrid.insertion_sort,), (dg.DataGrid.selection_sort,), (dg.DataGrid.merge_sort,), (dg.DataGrid.heap_sort,),(dg.DataGrid.quick_sort,),(dg.DataGrid.radix_sort,) )            
-timer.get_execution_time(dg.DataGrid.insertion_sort, True)
+results = analise_sort(samples, columns_datagrid, sorts)
+print(results)
